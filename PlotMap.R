ac5b0af3407fc8d9224b3eec5400c892f9680ef1
mandalsubhajit <mandalsubhajit@gmail.com>
2016-01-07 06:36:32 +0530

    Changed plot title and corrected the directory name

--- PlotMap.R
+++ PlotMap.R
@@ -1,5 +1,5 @@
 library(rworldmap)
-setwd("D:\\Github\\Inflation WorldMap 2014")
+setwd("D:\\Github\\Inflation-WorldMap-2014")
 
 inflationData <- read.csv("data.csv")
 inflDF <- joinCountryData2Map( inflationData
@@ -10,7 +10,7 @@ mapDevice(device = "png") #create world map shaped window
 mapParams <- mapCountryData(inflDF
                ,nameColumnToPlot='Inflation'
                , addLegend='FALSE'
-               , mapTitle = 'Inflation across Countries (2014)')
+               , mapTitle = 'Inflation across the World (2014)')
 do.call( addMapLegend, c( mapParams
                           , legendLabels="all"
                           , legendWidth=0.5
